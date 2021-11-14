ece1b12b0760ac9acc72af464d9e7c5299bd15de
Al-Allouhs <ahmad.alloush@gmail.com>
2021-11-14 18:40:23 +0100

    ignore properties if not exit in entity class

--- src/main/java/com/dachser/Entity/Address.java
+++ src/main/java/com/dachser/Entity/Address.java
@@ -1,5 +1,8 @@
 package com.dachser.Entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Address {
 	
 	private String street;
